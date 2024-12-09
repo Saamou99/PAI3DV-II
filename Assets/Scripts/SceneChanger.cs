2f97d848bd9267b979316590c6609b7a1c3317f0
Saamou <90028820+Saamou99@users.noreply.github.com>
2024-12-09 02:24:44 +0100

    Push 1

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName); // Load the scene with the specified name
+    }
+}
